2f811f16a3552eb1d2e9cf75cedf6d2cd4b667e6
vineboneto <vineboneto@gmail.com>
2021-10-03 19:32:44 -0300

    chore: update requirements

--- src/application/validator/validator_adapter.py
+++ src/application/validator/validator_adapter.py
@@ -3,7 +3,7 @@ from schema import Schema
 
 def validator_adapter(input: dict, schema: Schema) -> Exception:
     try:
-        schema.validate(input)
+        schema.validate(input._asdict())
         return None
     except Exception as e:
         return e
